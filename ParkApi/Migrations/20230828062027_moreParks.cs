8a1d33746d1f19f23a974b5314b2fca9cff766d6
Eva Kemp <eva.j.kemp@gmail.com>
2023-08-27 23:21:14 -0700

    add more parks to database

---
+++ ParkApi/Migrations/20230828062027_moreParks.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParkApi.Migrations
+{
+    public partial class moreParks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Location", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 6, "Washington", "Beacon Rock State Park", "State" },
+                    { 7, "Wyoming", "Yellowstone National Park", "National" },
+                    { 8, "Maine", "Acadia National Park", "National" },
+                    { 9, "Colorado", "Great Sand Dunes National Park", "National" },
+                    { 10, "Oregon", "Hat Rock State Park", "State" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 10);
+        }
+    }
+}
--- ParkApi/Models/ParkApiContext.cs
+++ ParkApi/Models/ParkApiContext.cs
@@ -2,24 +2,29 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ParkApi.Models
 {
-    public class ParkApiContext : DbContext
-    {
-        public DbSet<Park> Parks { get; set; }
+  public class ParkApiContext : DbContext
+  {
+    public DbSet<Park> Parks { get; set; }
 
-        public ParkApiContext(DbContextOptions<ParkApiContext> options) : base(options)
-        {
-        }
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            builder.Entity<Park>()
-              .HasData(
-                new Park { ParkId = 1, Name = "Crater Lake National Park", Location = "Oregon", Type = "National" },
-                new Park { ParkId = 2, Name = "Zion National Park", Location = "Utah", Type = "National" },
-                new Park { ParkId = 3, Name = "Smith Rock State Park", Location = "Oregon", Type = "State" },
-                new Park { ParkId = 4, Name = "Yosemite National Park", Location = "California", Type = "National" },
-                new Park { ParkId = 5, Name = "Ponderosa State Park", Location = "Idaho", Type = "State" }
-              );
-        }
+    public ParkApiContext(DbContextOptions<ParkApiContext> options) : base(options)
+    {
     }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Park>()
+        .HasData(
+          new Park { ParkId = 1, Name = "Crater Lake National Park", Location = "Oregon", Type = "National" },
+          new Park { ParkId = 2, Name = "Zion National Park", Location = "Utah", Type = "National" },
+          new Park { ParkId = 3, Name = "Smith Rock State Park", Location = "Oregon", Type = "State" },
+          new Park { ParkId = 4, Name = "Yosemite National Park", Location = "California", Type = "National" },
+          new Park { ParkId = 5, Name = "Ponderosa State Park", Location = "Idaho", Type = "State" },
+          new Park { ParkId = 6, Name = "Beacon Rock State Park", Location = "Washington", Type = "State" },
+          new Park { ParkId = 7, Name = "Yellowstone National Park", Location = "Wyoming", Type = "National" },
+          new Park { ParkId = 8, Name = "Acadia National Park", Location = "Maine", Type = "National" },
+          new Park { ParkId = 9, Name = "Great Sand Dunes National Park", Location = "Colorado", Type = "National" },
+          new Park { ParkId = 10, Name = "Hat Rock State Park", Location = "Oregon", Type = "State" }
+        );
+    }
+  }
 
 }
