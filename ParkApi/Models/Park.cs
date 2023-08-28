f207c08629eba284bf48416c64ddf07b5df02ed5
Eva Kemp <eva.j.kemp@gmail.com>
2023-08-28 07:54:14 -0700

    add required to models

--- ParkApi/Models/Park.cs
+++ ParkApi/Models/Park.cs
@@ -4,9 +4,13 @@ namespace ParkApi.Models
 {
     public class Park
     {
+        [Required(ErrorMessage = "The Name field is required.")]
         public int ParkId { get; set; }
+        [Required(ErrorMessage = "The Name field is required.")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "The Name field is required.")]
         public string Location { get; set; }
+        [Required(ErrorMessage = "The Name field is required.")]
         public string Type { get; set; }
     }
 }
